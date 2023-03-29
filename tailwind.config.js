f7fc0e1f37c16db068e4989382bd9e5a9b4370ad
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2023-03-29 18:56:54 +0900

    [add] 컴포넌트, 라이브러리 폴더 생성 및 tailwind세팅-1

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pantone: "#6667AB",
+        "pantone-light": "#A3A5D9",
+        "pantone-lighter": "#C2C2E0",
+        "pantone-lightest": "#E6E6F2",
+        "pantone-dark": "#3D3F6F",
+        "pantone-darker": "#2A2B4A",
+        "pantone-darkest": "#1A1A2E",
+      },
+    },
+  },
+  darkMode: "media", // class
+  plugins: [require("@tailwindcss/forms")],
+};
