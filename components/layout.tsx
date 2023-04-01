7bb64cff19a7c86ad46c308319c174d507f1a3eb
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2023-04-01 16:03:56 +0900

    [add] 레이아웃 로고추가

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { cls } from "@libs/client/utils";
 import Link from "next/link";
+import Image from "next/image";
+import logoImage from "../public/logo.png";
 import { useRouter } from "next/dist/client/router";
 
 interface LayoutProps {
@@ -30,28 +32,20 @@ export default function Layout({ title, children }: LayoutProps) {
       {/* Header */}
       <div
         className={
-          "z-10 bg-pantone w-full h-12 max-w-[390px] text-lg px-10 font-medium fixed text-white border-b tp-0 flex items-center"
+          "z-10 bg-white w-full h-12 max-w-[390px] text-lg px-10 font-medium fixed text-white border-b-2 tp-0 flex items-center"
         }
       >
-        <button
-          onClick={onClick}
+        <Link
+          href={""}
           className={"absolute left-4"}
         >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M15 19l-7-7 7-7"
-            />
-          </svg>
-        </button>
+          <Image
+            src={logoImage}
+            width={50}
+            // height={20}
+            alt={"소고기"}
+          ></Image>
+        </Link>
 
         <span className={"mx-2 text-xl font-bold"}>{title}</span>
 
