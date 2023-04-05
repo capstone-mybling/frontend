94951c0ea2e21ca3b62124ef8940dcf293b39b7c
hazzun <gkwns0516@gmail.com>
2023-04-05 22:55:50 +0900

    [EDIT] detail post 페이지 ui수정 작업

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import data from "../../data.json";
 import Layout from "../../components/layout";
 import Image from "next/image";
+import { AiOutlineHeart } from "react-icons/ai";
 
 // mui import
 import * as React from "react";
@@ -9,9 +10,6 @@ import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Boxes from "@mui/joy/Box";
-import Button from "@mui/joy/Button";
-import Textarea from "@mui/joy/Textarea";
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -58,6 +56,8 @@ interface Post {
 const Post = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [like, setLike] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
   // id에 해당하는 데이터를 찾아서 보여주는 로직
   const post = data.find((post: any) => post.id === Number(id));
@@ -68,8 +68,6 @@ const Post = () => {
   }
 
   // mui import - tabs
-  const [value, setValue] = React.useState(0);
-
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -99,10 +97,14 @@ const Post = () => {
           </div>
         </div>
         {/* post info : 좋아요, 공유, 가격 */}
-        <div className="flex flex-wrap px-2 py-4">
+        <div className="flex px-2 py-4">
           <div className="pr-2">
-            <button className="bg-white border-black border-2 rounded-full p-2">
-              좋아요 : 1
+            <button
+              className="bg-white border-black border-2 rounded-full p-2 w-full flex"
+              onClick={() => setLike((n) => n + 1)}
+            >
+              <AiOutlineHeart className="mt-1" />
+              <span> {like}</span>
             </button>
           </div>
           <div className="mr-auto">
@@ -121,7 +123,7 @@ const Post = () => {
           <h1 className="font-bold text-orange-500">{post.title}</h1>
           <p>{post.body}</p>
         </div>
-        {/* 댓글보기 */}
+        {/* 좋아요, 댓글, 알람설정도...? */}
         <div>
           <Box sx={{ width: "100%" }}>
             <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
@@ -148,29 +150,16 @@ const Post = () => {
         </div>
         {/* 댓글달기 */}
         <div>
-          <Boxes
-            sx={{
-              py: 2,
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              alignItems: "center",
-              flexWrap: "wrap",
-            }}
-          >
-            <form
-              onSubmit={(event) => {
-                event.preventDefault();
-              }}
-            >
-              <Textarea
-                placeholder="Try to submit with no text!"
-                required
-                sx={{ mb: 1 }}
+          <div>
+            <form className="flex border-t border-neutral-300 p-3">
+              <input
+                className="w-full ml-2 border-none outline-none"
+                type="text"
+                placeholder="Add a comment..."
               />
-              <Button type="submit">Submit</Button>
+              <button className="font-bold text-sky-500 ml-2">Post</button>
             </form>
-          </Boxes>
+          </div>
         </div>
       </div>
     </Layout>
