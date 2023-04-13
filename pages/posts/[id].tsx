8e91b5878c1238d720d9ec805c43c15db78d632c
hazzun <gkwns0516@gmail.com>
2023-04-13 23:17:48 +0900

    [DEL]기존에 작업한 tab기능 삭제

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,53 +2,12 @@ import { useRouter } from "next/router";
 import data from "../../data.json";
 import Layout from "../../components/layout";
 import Image from "next/image";
-import { AiOutlineHeart } from "react-icons/ai";
-
-// mui import
 import * as React from "react";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import HeartIcon from "@/components/icons/HeartIcons";
 import HeartFillIcon from "@/components/icons/HeartFillIcon";
 import ToggleButton from "@/components/ToggleButton";
 import CommentIcon from "@/components/icons/CommentIcon";
 import CommentFillIcon from "@/components/icons/CommentFillIcon";
-
-type TabPanelProps = {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-};
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-}
-//
 
 type Post = {
   userId: number;
@@ -63,7 +22,6 @@ const Post = () => {
   const { id } = router.query;
   const [like, setLike] = React.useState(false);
   const [comment, setComment] = React.useState(false);
-  // const [value, setValue] = React.useState(0);
 
   // id에 해당하는 데이터를 찾아서 보여주는 로직
   const post = data.find((post: any) => post.id === Number(id));
@@ -73,17 +31,12 @@ const Post = () => {
     return <div>post 데이터 읽어오지 못함</div>; // post가 undefined이면 로딩 메시지를 표시
   }
 
-  // mui import - tabs
-  // const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-  //   setValue(newValue);
-  // };
-  //
-
   const isFooterVisible = false;
 
   return (
     <Layout hasTabBar isFooterVisible={isFooterVisible}>
       <div className="py-4 px-4 max-w-mx-auto shadow-sm space-y-2 flex flex-row items-start space-x-10 justify-between">
+        {/* header info : 사용자 정보 & 작성시간 */}
         <div className="flex flex-1 items-center justify-between">
           <div className="flex items-center gap-4">
             <div className="w-10 h-10 border border-solid bg-slate-400 border-black rounded-full"></div>
@@ -108,7 +61,6 @@ const Post = () => {
         </div>
       </div>
       <div className="flex flex-col">
-        {/* header info : 사용자 정보 & 구매하기 */}
         {/* post info : 좋아요, 공유, 가격 */}
         <div className="flex px-2 py-4 justify-center gap-[150px]">
           <div className="">
@@ -143,31 +95,6 @@ const Post = () => {
           <h1 className="font-bold text-orange-500">{post.title}</h1>
           <p>{post.body}</p>
         </div>
-        {/* <div>
-          <Box sx={{ width: "100%" }}>
-            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-              <Tabs
-                value={value}
-                onChange={handleChange}
-                aria-label="basic tabs example"
-              >
-                <Tab label="댓글" {...a11yProps(0)} />
-                <Tab label="좋아요" {...a11yProps(1)} />
-                <Tab label="뭐넣지 .." {...a11yProps(2)} />
-              </Tabs>
-            </Box>
-            <TabPanel value={value} index={0}>
-              댓글달면 자동으로 올라가는 칸
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              좋아요 누른사람들 뜨는 칸
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-              뭐 넣을지 모르는 칸
-            </TabPanel>
-          </Box>
-        </div> */}
-        {/* 댓글달기 */}
         <div>
           <div className="sticky bottom-0 z-20">
             <form className="flex border-t border-neutral-300 p-3">
