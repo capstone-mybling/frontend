18542979bb7f6fa2385fcf1e1c537e665841c779
hazzun <gkwns0516@gmail.com>
2023-04-19 18:50:51 +0900

    [ADD]프로필, 디테일 페이지 tabs 추가 - npm i 후 실행바람

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import data from "../../data.json";
 import Layout from "@/components/Layout";
@@ -9,7 +9,11 @@ import ToggleButton from "@/components/ToggleButton";
 import CommentIcon from "@/components/icons/CommentIcon";
 import CommentFillIcon from "@/components/icons/CommentFillIcon";
 import UserAvatar from "@/components/UserAvatar";
-import { useEffect } from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import Tabs from "@mui/material/Tabs";
+import TabPanel from "@mui/lab/TabPanel";
 
 type Post = {
   userId: number;
@@ -36,6 +40,13 @@ export default function Post() {
 
   const likeUp = () => {
     setLikeCount(likeCount === 5 ? ++likeCount : --likeCount);
+  };
+
+  // MUI tabs
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
   };
 
   const [comments, setComments] = useState([
@@ -134,46 +145,67 @@ export default function Post() {
             <h1 className="font-bold text-orange-500">{post.title}</h1>
             <p>{post.body}</p>
           </div>
-          {/* 댓글 리스트 */}
-          <div className="border-t-2 mt-4 pt-4">
-            <ul>
-              {comments.map((comment) => (
-                <li
-                  key={comment.id}
-                  className="flex justify-between px-5 flex-col pb-4"
+          <Box sx={{ width: "100%", typography: "body1" }}>
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  textColor="secondary"
+                  indicatorColor="secondary"
+                  aria-label="secondary tabs example"
                 >
-                  <UserAvatar
-                    size="small"
-                    UserName={"userName"}
-                    UserImage={
-                      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    }
-                  />
-                  <div className="ml-11">{comment.content}</div>
-                  <div className="flex justify-between items-center">
-                    <div className="text-gray-500 text-sm ml-11">
-                      5 minutes ago
-                    </div>
-                    <div className="flex items-center">
-                      <p>10</p>
-                      <ToggleButton
-                        toggled={commentLike}
-                        onToggle={setCommentLike}
-                        onIcon={<HeartFillIcon />}
-                        offIcon={<HeartIcon />}
-                      />
-                      <button
-                        className="text-violet-200 ml-2 text-2xl"
-                        onClick={() => handleDeleteComment(comment.id)}
+                  <Tab label="댓글" value="1" />
+                  <Tab label="거래정보?" value="2" />
+                  <Tab label="NFT 정보?" value="3" />
+                </Tabs>
+              </Box>
+              <TabPanel value="1" sx={{ padding: 0 }}>
+                <div className="mt-4 pt-4">
+                  <ul>
+                    {comments.map((comment) => (
+                      <li
+                        key={comment.id}
+                        className="flex justify-between px-5 flex-col pb-4"
                       >
-                        X
-                      </button>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+                        <UserAvatar
+                          size="small"
+                          UserName={"userName"}
+                          UserImage={
+                            "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          }
+                        />
+                        <div className="ml-11">{comment.content}</div>
+                        <div className="flex justify-between items-center">
+                          <div className="text-gray-500 text-sm ml-11">
+                            5 minutes ago
+                          </div>
+                          <div className="flex items-center">
+                            <p>10</p>
+                            <ToggleButton
+                              toggled={commentLike}
+                              onToggle={setCommentLike}
+                              onIcon={<HeartFillIcon />}
+                              offIcon={<HeartIcon />}
+                            />
+                            <button
+                              className="text-violet-200 ml-2 text-2xl"
+                              onClick={() => handleDeleteComment(comment.id)}
+                            >
+                              X
+                            </button>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </TabPanel>
+              <TabPanel value="2">앙냥냥</TabPanel>
+              <TabPanel value="3">띵띵땅땅띵~</TabPanel>
+            </TabContext>
+          </Box>
+          {/* 댓글 리스트 */}
         </div>
       </Layout>
       <footer className="sticky bottom-0 bg-white z-10 border-b">
