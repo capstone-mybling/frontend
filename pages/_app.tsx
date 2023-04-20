902bcf2e65550aad5058b5b65bc30bea3cdf9f53
KSG072 <tjrnjs123@gmail.com>
2023-04-20 14:05:39 +0900

    [ADD] 기본 폰트 네이버 나눔 고딕 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
+import { cls } from "@/libs/client/utils";
 import type { AppProps } from "next/app";
+import {Nanum_Gothic} from 'next/font/google'
 
+const nanum = Nanum_Gothic({subsets: ['latin'], weight:"400"})
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div
       className={
-        "mx-auto my-auto flex-col min-h-screen bg-white flex justify-between"
+        cls("mx-auto my-auto flex-col min-h-screen bg-white flex justify-between", nanum.className)
       }
     >
       <Component {...pageProps} />
