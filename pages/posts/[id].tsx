014050c8f78c2979f70c0ab7f9832a718e9f9259
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2023-04-21 18:04:28 +0900

    [EDIT] 헤더가 화면 양 끝까지 펼쳐지는 버그 해결

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,7 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState, useEffect, Fragment } from "react";
 import { useRouter } from "next/router";
 import data from "../../data.json";
-import Layout from "@/components/Layout";
+import Layout from "@components/Layout";
 import Image from "next/image";
 import HeartIcon from "@/components/icons/HeartIcons";
 import HeartFillIcon from "@/components/icons/HeartFillIcon";
@@ -35,8 +35,6 @@ export default function Post() {
   useEffect(() => {
     inputRef.current?.focus();
   }, [comment]);
-
-  const isFooterVisible = false;
 
   const likeUp = () => {
     setLikeCount(likeCount === 5 ? ++likeCount : --likeCount);
@@ -83,131 +81,143 @@ export default function Post() {
   // id에 해당하는 데이터를 찾아서 보여주는 로직
   const post = data.find((post: any) => post.id === Number(id));
   // console.log("post나오나? -> ", post);
-
-  if (!post) {
-    return <div>post 데이터 읽어오지 못함</div>; // post가 undefined이면 로딩 메시지를 표시
-  }
-
   return (
-    <>
-      <Layout hasTabBar isFooterVisible={isFooterVisible}>
-        <div className="py-4 px-4 max-w-mx-auto shadow-sm space-y-2 flex flex-row items-start space-x-10 justify-between">
-          {/* header info : 사용자 정보 & 작성시간 */}
-          <div className="flex flex-1 items-center justify-between">
-            <UserAvatar
-              size="medium"
-              UserName={"userName"}
-              UserImage={
-                "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-              }
-            />
-            <p>5 minutes ago</p>
-          </div>
-        </div>
-        <div className="relative">
-          <div className="flex justify-center align-middle py-6 bg-gray-200">
-            <Image
-              src={`/${post.image}`}
-              alt={post.title}
-              width="400"
-              height="400"
-            />
-          </div>
-          <div className="absolute bottom-2 right-2">
-            <button className="bg-black opacity-30 px-6 py-1 rounded-2xl text-white hover:opacity-70">
-              <a href="https://opensea.io/">구매하기</a>
-            </button>
-          </div>
-        </div>
-        {/* post info : 좋아요, 댓글 */}
-        <div className="flex flex-col px-2 py-4 justify-center">
-          <div className="flex flex-row justify-evenly">
-            <div className="flex items-center" onClick={likeUp}>
-              <ToggleButton
-                toggled={like}
-                onToggle={setLike}
-                onIcon={<HeartFillIcon />}
-                offIcon={<HeartIcon />}
+    <Layout disableFooter>
+      {!post ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Fragment>
+          <div className="py-4 px-4 mx-auto shadow-sm space-y-2 flex flex-row items-start space-x-10 justify-between">
+            {/* header info : 사용자 정보 & 작성시간 */}
+            <div className="flex flex-1 items-center justify-between">
+              <UserAvatar
+                size="medium"
+                UserName={"userName"}
+                UserImage={
+                  "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                }
               />
-              <span>{likeCount}</span>
-            </div>
-            <div>
-              <ToggleButton
-                toggled={comment}
-                onToggle={setComment}
-                onIcon={<CommentFillIcon />}
-                offIcon={<CommentIcon />}
-              />
+              <p>5 minutes ago</p>
             </div>
           </div>
-          {/* post detail : 게시글 상세정보 */}
-          <div className="py-4 px-4 ">
-            <h1 className="font-bold text-orange-500">{post.title}</h1>
-            <p>{post.body}</p>
+          <div className="relative">
+            <div className="flex justify-center align-middle py-6 bg-gray-200">
+              <Image
+                src={`/${post.image}`}
+                alt={post.title}
+                width="400"
+                height="400"
+              />
+            </div>
+            <div className="absolute bottom-2 right-2">
+              <button className="bg-black opacity-30 px-6 py-1 rounded-2xl text-white hover:opacity-70">
+                <a href="https://opensea.io/">구매하기</a>
+              </button>
+            </div>
           </div>
-          <Box sx={{ width: "100%", typography: "body1" }}>
-            <TabContext value={value}>
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  textColor="secondary"
-                  indicatorColor="secondary"
-                  aria-label="secondary tabs example"
+          {/* post info : 좋아요, 댓글 */}
+          <div className="flex flex-col px-2 py-4 justify-center">
+            <div className="flex flex-row justify-evenly">
+              <div
+                className="flex items-center"
+                onClick={likeUp}
+              >
+                <ToggleButton
+                  toggled={like}
+                  onToggle={setLike}
+                  onIcon={<HeartFillIcon />}
+                  offIcon={<HeartIcon />}
+                />
+                <span>{likeCount}</span>
+              </div>
+              <div>
+                <ToggleButton
+                  toggled={comment}
+                  onToggle={setComment}
+                  onIcon={<CommentFillIcon />}
+                  offIcon={<CommentIcon />}
+                />
+              </div>
+            </div>
+            {/* post detail : 게시글 상세정보 */}
+            <div className="py-4 px-4 ">
+              <h1 className="font-bold text-orange-500">{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+            <Box sx={{ width: "100%", typography: "body1" }}>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                  <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    textColor="secondary"
+                    indicatorColor="secondary"
+                    aria-label="secondary tabs example"
+                  >
+                    <Tab
+                      label="댓글"
+                      value="1"
+                    />
+                    <Tab
+                      label="거래정보?"
+                      value="2"
+                    />
+                    <Tab
+                      label="NFT 정보?"
+                      value="3"
+                    />
+                  </Tabs>
+                </Box>
+                <TabPanel
+                  value="1"
+                  sx={{ padding: 0 }}
                 >
-                  <Tab label="댓글" value="1" />
-                  <Tab label="거래정보?" value="2" />
-                  <Tab label="NFT 정보?" value="3" />
-                </Tabs>
-              </Box>
-              <TabPanel value="1" sx={{ padding: 0 }}>
-                <div className="mt-4 pt-4">
-                  <ul>
-                    {comments.map((comment) => (
-                      <li
-                        key={comment.id}
-                        className="flex justify-between px-5 flex-col pb-4"
-                      >
-                        <UserAvatar
-                          size="small"
-                          UserName={"userName"}
-                          UserImage={
-                            "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          }
-                        />
-                        <div className="ml-11">{comment.content}</div>
-                        <div className="flex justify-between items-center">
-                          <div className="text-gray-500 text-sm ml-11">
-                            5 minutes ago
+                  <div className="mt-4 pt-4">
+                    <ul>
+                      {comments.map((comment) => (
+                        <li
+                          key={comment.id}
+                          className="flex justify-between px-5 flex-col pb-4"
+                        >
+                          <UserAvatar
+                            size="small"
+                            UserName={"userName"}
+                            UserImage={
+                              "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            }
+                          />
+                          <div className="ml-11">{comment.content}</div>
+                          <div className="flex justify-between items-center">
+                            <div className="text-gray-500 text-sm ml-11">5 minutes ago</div>
+                            <div className="flex items-center">
+                              <p>10</p>
+                              <ToggleButton
+                                toggled={commentLike}
+                                onToggle={setCommentLike}
+                                onIcon={<HeartFillIcon />}
+                                offIcon={<HeartIcon />}
+                              />
+                              <button
+                                className="text-violet-200 ml-2 text-2xl"
+                                onClick={() => handleDeleteComment(comment.id)}
+                              >
+                                X
+                              </button>
+                            </div>
                           </div>
-                          <div className="flex items-center">
-                            <p>10</p>
-                            <ToggleButton
-                              toggled={commentLike}
-                              onToggle={setCommentLike}
-                              onIcon={<HeartFillIcon />}
-                              offIcon={<HeartIcon />}
-                            />
-                            <button
-                              className="text-violet-200 ml-2 text-2xl"
-                              onClick={() => handleDeleteComment(comment.id)}
-                            >
-                              X
-                            </button>
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </TabPanel>
-              <TabPanel value="2">앙냥냥</TabPanel>
-              <TabPanel value="3">띵띵땅땅띵~</TabPanel>
-            </TabContext>
-          </Box>
-          {/* 댓글 리스트 */}
-        </div>
-      </Layout>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </TabPanel>
+                <TabPanel value="2">앙냥냥</TabPanel>
+                <TabPanel value="3">띵띵땅땅띵~</TabPanel>
+              </TabContext>
+            </Box>
+            {/* 댓글 리스트 */}
+          </div>
+        </Fragment>
+      )}
       <footer className="sticky bottom-0 bg-white z-10 border-b">
         <div>
           <form
@@ -233,7 +243,7 @@ export default function Post() {
           </form>
         </div>
       </footer>
-    </>
+    </Layout>
   );
 }
 
