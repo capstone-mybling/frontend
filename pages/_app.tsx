a79eaa5f22f18229c5b20ccd756bad8e6c76f81c
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2023-05-13 17:24:15 +0900

    [EDIT] 리액트 쿼리 초기 세팅

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import "@/styles/globals.css";
 import { cls } from "@/libs/client/utils";
 import type { AppProps } from "next/app";
-import {Nanum_Gothic} from 'next/font/google'
+import { Nanum_Gothic } from "next/font/google";
+import { QueryClientProvider, QueryClient } from "react-query";
 
-const nanum = Nanum_Gothic({subsets: ['latin'], weight:"400"})
+const nanum = Nanum_Gothic({ subsets: ["latin"], weight: "400" });
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
-    <div
-      className={
-        cls("mx-auto my-auto flex-col min-h-screen bg-white flex justify-between", nanum.className)
-      }
-    >
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div
+        className={cls(
+          "mx-auto my-auto flex-col min-h-screen bg-white flex justify-between",
+          nanum.className
+        )}
+      >
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
   );
 }
