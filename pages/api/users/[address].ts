d01f1c520e4e1dbc81aca5dc24b35650afd5a7e3
hajoon <gkwns0516@gmail.com>
2023-05-15 00:29:46 +0900

    [EDIT] 팔로우/팔로잉 목록에 오브젝트 형식으로 들어가게끔 수정(이유는 상세설명에)
    기존 mypage에서 팔로워/팔로잉 모달창과 같이  other user page에서도 팔로워/팔로잉 모달창에 같은 내용을 props로 담아서 넘겨줘야하기 때문에

--- pages/api/users/[address].ts
+++ pages/api/users/[address].ts
@@ -1,57 +1,89 @@
-import type {NextApiRequest, NextApiResponse} from 'next'
-import {withApiSession} from "@libs/server/withSession";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
 import withHandler from "@libs/server/withHandler";
 import baseResponse from "@libs/server/response";
 import client from "@libs/server/client";
-import {User} from '@prisma/client';
+import { User } from "@prisma/client";
 import ErrorCode from "@libs/server/error_code";
 import getRedisClient from "@libs/server/redis";
 
 interface UserWithFollow extends User {
-    followings: string[];
-    followers: string[];
+  followings: string[];
+  followers: string[];
 }
 
 const handler = async (
-    request: NextApiRequest,
-    response: NextApiResponse<any>
+  request: NextApiRequest,
+  response: NextApiResponse<any>
 ) => {
-    const {address} = request.query;
+  const { address } = request.query;
 
-    if (!address) {
-        baseResponse(response, {
-            success: false,
-            error: {
-                message: "not found address",
-                errorMessage: "주소가 존재하지 않습니다. 다시 입력해주세요.",
-                errorCode: ErrorCode.ADDRESS_IS_REQUIRED,
-            }
-        })
-    }
-
-    const findUser = await client.user.findUnique({
-        where: {
-            address: address as string,
-        }
-    });
-
-    const redis = await getRedisClient();
-    const userFollowers = await redis.sMembers(`user:${address}:followers`);
-    const userFollowings = await redis.sMembers(`user:${address}:followings`);
-
+  if (!address) {
     baseResponse(response, {
-        success: true,
-        data: {
-            ...findUser,
-            followers: userFollowers,
-            followings: userFollowings,
-        }
+      success: false,
+      error: {
+        message: "not found address",
+        errorMessage: "주소가 존재하지 않습니다. 다시 입력해주세요.",
+        errorCode: ErrorCode.ADDRESS_IS_REQUIRED,
+      },
+    });
+  }
+
+  const findUser = await client.user.findUnique({
+    where: {
+      address: address as string,
+    },
+  });
+
+  const redis = await getRedisClient();
+  const userFollowers = await redis.sMembers(`user:${address}:followers`);
+  const userFollowings = await redis.sMembers(`user:${address}:followings`);
+
+  const userFollowerWithInfo = await Promise.all(
+    userFollowers.map(async (userAddress) => {
+      const user = await client.user.findUnique({
+        where: {
+          address: userAddress,
+        },
+      });
+      if (!user) return;
+      return {
+        address: userAddress,
+        avatar: user.avatar,
+        username: user.username,
+      };
     })
-}
+  );
+  const userFollowingWithInfo = await Promise.all(
+    userFollowings.map(async (userAddress) => {
+      const user = await client.user.findUnique({
+        where: {
+          address: userAddress,
+        },
+      });
+      if (!user) return;
+      return {
+        address: userAddress,
+        avatar: user.avatar,
+        username: user.username,
+      };
+    })
+  );
+
+  baseResponse(response, {
+    success: true,
+    data: {
+      ...findUser,
+      followers: userFollowerWithInfo,
+      followings: userFollowingWithInfo,
+    },
+  });
+};
 
 export default withApiSession(
-    withHandler({
-        methods: ["GET"],
-        handler,
-        isPrivate: false,
-    }));
+  withHandler({
+    methods: ["GET"],
+    handler,
+    isPrivate: false,
+  })
+);
