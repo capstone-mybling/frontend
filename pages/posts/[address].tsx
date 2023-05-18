ca120157a3a8a608770125917a68283aaec50ef3
Kim-Seo-Gwon <tjrnjs123@gmail.com>
2023-05-18 17:55:44 +0900

    [EDIT] 상세 페이지 게시글 조회 API 연동

--- pages/posts/[address].tsx
+++ pages/posts/[address].tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect, Fragment } from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, GetServerSidePropsResult, NextPage } from "next";
 import { useRouter } from "next/router";
 import Layout from "@components/Layout";
 import Image from "next/image";
@@ -14,68 +14,73 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import Tabs from "@mui/material/Tabs";
 import TabPanel from "@mui/lab/TabPanel";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { Post, PostComment, User } from "@/libs/client/types";
 
-const Home: NextPage = () => {
-  const router = useRouter();
-  const { address } = router.query;
-  const [like, setLike] = useState<boolean>(false);
-  const [comment, setComment] = useState<boolean>(false);
-  const [commentLike, setCommentLike] = useState<boolean>(false);
-  let [likeCount, setLikeCount] = useState<number>(5);
-  const inputRef = useRef<HTMLInputElement>(null);
+interface DetailPost extends Post {
+  author: User;
+  comments: PostComment[];
+}
 
-  useEffect(() => {
-    inputRef.current?.focus();
-  }, [comment]);
-
-  const likeUp = () => {
-    setLikeCount(likeCount === 5 ? ++likeCount : --likeCount);
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  console.log(context.query);
+  return {
+    props: {
+      address: context.query.address,
+    },
   };
+};
 
+interface HomeProps {
+  address: string;
+}
+
+const Home = ({ address }: HomeProps) => {
+  // const router = useRouter();
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+  const [tabIndex, setTabIndex] = useState("1");
+  //calling API and handling data
+  const { data, status } = useQuery<DetailPost>(
+    "post",
+    async () => await axios.get(`/api/posts/${address}`).then((res) => res.data.data)
+  );
+  console.log(data);
   // MUI tabs
-  const [value, setValue] = useState("1");
-
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
+  const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
+    setTabIndex(newValue);
   };
-
-  const [comments, setComments] = useState([
-    { id: 1, content: "노하준 폼 미쳤다" },
-    { id: 2, content: "ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ" },
-    { id: 3, content: "인절미슈페너에 샷추가" },
-  ]);
-  const [newComment, setNewComment] = useState("");
-
-  // 댓글 추가
-  const handleAddComment = () => {
-    const newId = comments.length + 1;
-    const newCommentObj = { id: newId, content: newComment };
-    setComments([...comments, newCommentObj]);
-    setNewComment("");
-    // console.log(comments);
+  const handlePostLike = () => {
+    setIsLiked(!isLiked);
   };
-  const handleBlockComment = () => {
-    console.log("block!");
-    // e.preventDefault();
-    inputRef.current?.focus();
-  };
+  // console.log(data);
+  // // 댓글 추가
+  // const handleAddComment = () => {
+  //   const newId = comments.length + 1;
+  //   const newCommentObj = { id: newId, content: newComment };
+  //   setComments([...comments, newCommentObj]);
+  //   setNewComment("");
+  //   // console.log(comments);
+  // };
+  // const handleBlockComment = () => {
+  //   console.log("block!");
+  //   // e.preventDefault();
+  //   inputRef.current?.focus();
+  // };
 
-  // 댓글 삭제
-  const handleDeleteComment = (id: number) => {
-    const updatedComments = comments.filter((comment) => comment.id !== id);
-    setComments(updatedComments);
-  };
+  // // 댓글 삭제
+  // const handleDeleteComment = (id: number) => {
+  //   const updatedComments = comments.filter((comment) => comment.id !== id);
+  //   setComments(updatedComments);
+  // };
 
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-  };
+  // const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  // };
 
-  // id에 해당하는 데이터를 찾아서 보여주는 로직
-  const post = data.find((post: any) => post.id === Number(id));
-  // console.log("post나오나? -> ", post);
   return (
     <Layout disableFooter>
-      {!post ? (
+      {status !== "success" ? (
         <h2>Loading...</h2>
       ) : (
         // Fragment태그는 <>(빈 태그)와 같은 기능을 가진 태그라고 함 - by 준수형
@@ -85,9 +90,12 @@ const Home: NextPage = () => {
             <div className="flex flex-1 items-center justify-between">
               <UserAvatar
                 size="medium"
-                UserName={"userName"}
+                UserAddr={data.authorAddress}
+                UserName={data.author.username!}
                 UserImage={
-                  "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  data.author.avatar
+                    ? data.author.avatar
+                    : "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 }
               />
               <button className="bg-black opacity-30 px-6 py-1 rounded-2xl text-white hover:opacity-70">
@@ -95,12 +103,13 @@ const Home: NextPage = () => {
               </button>
             </div>
           </div>
-          <div className="flex justify-center align-middle py-6 bg-gray-200">
+          <div className="flex justify-center align-middle py-6 ">
             <Image
-              src={`/${post.image}`}
-              alt={post.title}
-              width="400"
-              height="400"
+              className="h-full x-auto block object-cover m-0"
+              src={`${data.thumbnail}`}
+              alt={data.name}
+              width="2000"
+              height="2000"
             />
           </div>
           {/* post info : 좋아요, 댓글 */}
@@ -113,12 +122,12 @@ const Home: NextPage = () => {
               </div>
               <div
                 className="flex items-center justify-end my-3"
-                onClick={likeUp}
+                // onClick={likeUp}
               >
-                <span>{likeCount}</span>
+                <span>{3}</span>
                 <ToggleButton
-                  toggled={like}
-                  onToggle={setLike}
+                  toggled={isLiked}
+                  onToggle={handlePostLike}
                   onIcon={<HeartFillIcon />}
                   offIcon={<HeartIcon />}
                 />
@@ -128,27 +137,20 @@ const Home: NextPage = () => {
             <section className="px-2">
               <div className="flex justify-between items-center">
                 <div>
-                  <h1 className="font-bold text-2xl">title????</h1>
+                  <h1 className="font-bold text-2xl">{data.name}</h1>
                 </div>
                 <div>
-                  <p className="text-gray-500">5 minutes ago</p>
+                  <p className="text-gray-500">{data.updatedAt.toString()}</p>
                 </div>
               </div>
-              <p className="my-4">
-                책읽는 심슨이 있습니다. 그러나, 책을 읽는 것은 심슨 가족 중에서는 극적으로 드문
-                일이며, 그들은 대부분 이상한 모험을 즐기며 시간을 보냅니다.책읽는 심슨이 있습니다.
-                그러나, 책을 읽는 것은 심슨 가족 중에서는 극적으로 드문 일이며, 그들은 대부분 이상한
-                모험을 즐기며 시간을 보냅니다.책읽는 심슨이 있습니다. 그러나, 책을 읽는 것은 심슨
-                가족 중에서는 극적으로 드문 일이며, 그들은 대부분 이상한 모험을 즐기며 시간을
-                보냅니다.
-              </p>
+              <p className="my-4">{data.description}</p>
             </section>
             <Box sx={{ width: "100%", typography: "body1" }}>
-              <TabContext value={value}>
+              <TabContext value={tabIndex}>
                 <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
                   <Tabs
-                    value={value}
-                    onChange={handleChange}
+                    value={tabIndex}
+                    onChange={handleTabChange}
                     textColor="secondary"
                     indicatorColor="secondary"
                     aria-label="secondary tabs example"
@@ -177,14 +179,15 @@ const Home: NextPage = () => {
                 >
                   <div className="mt-4 pt-4">
                     <ul>
-                      {comments.map((comment) => (
+                      {data.comments.map((comment) => (
                         <li
                           key={comment.id}
                           className="flex justify-between px-5 flex-col pb-4"
                         >
                           <UserAvatar
                             size="small"
-                            UserName={"userName"}
+                            UserAddr={comment.authorAddress}
+                            UserName={"1212"}
                             UserImage={
                               "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                             }
@@ -194,18 +197,18 @@ const Home: NextPage = () => {
                             <div className="text-gray-500 text-sm ml-11">5 minutes ago</div>
                             <div className="flex items-center">
                               <p>10</p>
-                              <ToggleButton
-                                toggled={commentLike}
-                                onToggle={setCommentLike}
-                                onIcon={<HeartFillIcon />}
-                                offIcon={<HeartIcon />}
-                              />
-                              <button
-                                className="text-violet-200 ml-2 text-2xl"
-                                onClick={() => handleDeleteComment(comment.id)}
-                              >
-                                X
-                              </button>
+                              {/* <ToggleButton
+                              toggled={commentLike}
+                              onToggle={setCommentLike}
+                              onIcon={<HeartFillIcon />}
+                              offIcon={<HeartIcon />}
+                            />
+                            <button
+                              className="text-violet-200 ml-2 text-2xl"
+                              onClick={() => handleDeleteComment(comment.id)}
+                            >
+                              X
+                            </button> */}
                             </div>
                           </div>
                         </li>
@@ -217,20 +220,20 @@ const Home: NextPage = () => {
                     <div>
                       <form
                         className="flex border-t border-neutral-300 p-3"
-                        onSubmit={onSubmit}
+                        // onSubmit={console.log("submit")}
                       >
                         <input
                           className="w-full ml-2 border-none outline-none"
                           type="text"
                           placeholder="Add a comment..."
-                          onChange={(e) => setNewComment(e.target.value)}
-                          ref={inputRef}
-                          value={newComment}
+                          // onChange={(e) => setNewComment(e.target.value)}
+                          // ref={inputRef}
+                          // value={newComment}
                         />
                         <button
                           className="font-bold text-violet-500 ml-2"
                           onClick={() => {
-                            newComment === "" ? handleBlockComment() : handleAddComment();
+                            // newComment === "" ? handleBlockComment() : handleAddComment();
                           }}
                         >
                           Post
